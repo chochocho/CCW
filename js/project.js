8df6dbbc66de614e3d51cab7ebc1935f2c0460f1
chochocho <platypus3333@gmail.com>
2015-03-30 00:22:13 -0400

    Shop and fixes

--- js/project.js
+++ js/project.js
@@ -1,10 +1,26 @@
+// Scroll-Toggle
+
 $(function() {
     $('#nav-trigger').click(function(){
         $('.nav').slideToggle(100);
     });
     $('#follow-trigger').click(function(){
-        $('.follow').slideToggle(100);
+        $('.follow-links').slideToggle(100);
     });
 });
 
+// Skrollr
+
 var s = skrollr.init();
+
+// Moment 
+
+var update = function () {
+    $('#day').html(moment().format('MMM. DD, YYYY'));
+    $('#time').html(moment().format('HH:mm:ss'));
+}
+
+$(document).ready(function(){
+    update();
+    setInterval(update, 1000);
+});

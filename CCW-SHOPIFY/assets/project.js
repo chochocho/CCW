ac2598db9ba35c2a01bdb3768dada302ee91957b
chochocho <platypus3333@gmail.com>
2015-04-17 00:59:48 -0400

    SHOPIFY

---
+++ CCW-SHOPIFY/assets/project.js
@@ -0,0 +1,22 @@
+// Scroll-Toggle
+
+$(function() {
+    $('#menu-trigger').click(function(){
+        $('.menu').slideToggle(100);
+    });
+    $('#social-trigger').click(function(){
+        $('.social-links').slideToggle(100);
+    });
+});
+
+// Moment 
+
+var update = function () {
+    $('#day').html(moment().format('MMM. DD, YYYY'));
+    $('#time').html(moment().format('HH:mm:ss'));
+}
+
+$(document).ready(function(){
+    update();
+    setInterval(update, 1000);
+});
